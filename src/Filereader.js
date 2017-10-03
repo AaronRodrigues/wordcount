a3f6e3e021b80c06d48aea13d7bcadeb0903560f
AaronRodrigues <aaronrod@gmail.com>
2017-10-03 21:20:04 +0100

    Prints word frequency in the view

--- src/Filereader.js
+++ src/Filereader.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 
   var fileInput = document.getElementById('fileInput');
-
+  var wordBody = document.getElementById('wordBody')
   fileInput.addEventListener('change', function () {
     var file = fileInput.files[0];
 
@@ -12,10 +12,20 @@ window.onload = function() {
       fileReader.onload = function() {
         var wordCount = new WordCount(fileReader.result); 
         var wordArray = wordCount.executeWordCount();
+        var wordBody = document.getElementById('wordBody');
         console.log(wordArray);
+        printWords(wordBody, wordArray);
       };
     }else {
 				fileDisplayArea.innerText = "File not supported!"
 		}
   });
+  
+  printWords = function(div, array) {
+    array = [];
+    for (var i = 0; i < array.length; i++) {
+      htmlArray.push("<p> <b>" + array[i][0]  + "</b> - " + array[i][1]  + "</p>");
+    }
+    div.innerHTML = array.join('');
+  }
 }
